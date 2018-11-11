e1b4b2f79e9580b7c4cd2282ce0366e7a63000e8
ste001 <asso99@hotmail.com>
2018-11-11 16:36:38 +0100

    Add a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,10 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
 
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => 
+    ["image/jpg", "image/jpeg", "image/png"]
+
   def tag_list
     self.tags.collect do |tag|
       tag.name
@@ -11,9 +15,8 @@ class Article < ApplicationRecord
 
   def tag_list=(tags_string)
     tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
-    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    new_or_found_tags = tag_names.collect{ |name| Tag.find_or_create_by(name: name) }
     self.tags = new_or_found_tags
-  end
-  
+  end  
   
 end
